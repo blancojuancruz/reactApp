badd8fa25480a8d0b5ff709aee61ed1cbc7e4ca4
blancojuancruz <99100776+blancojuancruz@users.noreply.github.com>
2022-07-01 20:29:21 -0300

    fix

--- src/Services/Firebase/FirebaseInit.jsx
+++ src/Services/Firebase/FirebaseInit.jsx
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: 'AIzaSyBr-saAHDhUeoCn2yUaqc123JRbVfMHPis',
+  authDomain: 'hardwarestore-2118c.firebaseapp.com',
+  projectId: 'hardwarestore-2118c',
+  storageBucket: 'hardwarestore-2118c.appspot.com',
+  messagingSenderId: '950217898644',
+  appId: '1:950217898644:web:ed8b790444fe911f98d281'
 }
 
 const app = initializeApp(firebaseConfig)
