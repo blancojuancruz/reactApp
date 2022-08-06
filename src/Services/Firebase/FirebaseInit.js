90b8db05cd3dd202252e0655a044d451b8d8b176
blancojuancruz <99100776+blancojuancruz@users.noreply.github.com>
2022-08-05 21:25:57 -0300

    testing firebase

--- src/Services/Firebase/FirebaseInit.js
+++ src/Services/Firebase/FirebaseInit.js
@@ -1,15 +1,15 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
-// const { REACT_APP_API_KEY, REACT_APP_AUTH_DOMAIN, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID, REACT_APP_APP_ID } = process.env
+const { REACT_APP_API_KEY, REACT_APP_PROJECT_ID, REACT_APP_STORAGE_BUCKET, REACT_APP_MESSAGING_SENDER_ID, REACT_APP_APP_ID } = process.env
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBr-saAHDhUeoCn2yUaqc123JRbVfMHPis',
+  apiKey: REACT_APP_API_KEY,
   authDomain: 'hardwarestore-2118c.firebaseapp.com',
-  projectId: 'hardwarestore-2118c',
-  storageBucket: 'hardwarestore-2118c.appspot.com',
-  messagingSenderId: '950217898644',
-  appId: '1:950217898644:web:ed8b790444fe911f98d281'
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID,
+  appId: REACT_APP_APP_ID
 }
 
 const app = initializeApp(firebaseConfig)
